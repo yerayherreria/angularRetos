3817e31b5d04b83af350c55e2714413271234154
Yeray Herrería Oña <yerayherre@gmail.com>
2023-12-20 14:00:44 +0100

    Reto acabado

--- src/app/botones/botones.component.ts
+++ src/app/botones/botones.component.ts
@@ -12,13 +12,10 @@ export class BotonesComponent {
 
   buttons=["😀","😢","🤪"];
   images =["https://i.pinimg.com/originals/f2/ce/c9/f2cec98f06e8f66ff0bcfb2ffdb413eb.jpg","https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sad-dog-1537347496.jpg?resize=480:*","https://townsquare.media/site/757/files/2014/08/names.jpg"]
-  boolean=[false,false,false]
-  viewImage(index: number): void {
-    if(this.boolean[index]==false){
-      this.boolean[index]=true;
-    } else {
-      this.boolean[index]=false;
-    }
+  selectedImageIndex: number = 0;
+
+  showImage(index: number): void {
+    this.selectedImageIndex = index;
   }
 
 }
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Product } from './product';
+import { StartsComponent } from '../starts/starts.component';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [StartsComponent],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
